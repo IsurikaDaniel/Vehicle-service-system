6804b0b1f34dd75678b75831101e4e407209489f
IsurikaDaniel <d.nethmiisurika@gmail.com>
2024-10-30 22:29:55 +0530

    nav bar click changes added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { CustomerAccountComponent } from './pages/customer-account/customer-account.component';
+import { DashbordComponent } from './pages/dashbord/dashbord.component';
+import { ServiceComponent } from './pages/service/service.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:"Home",
+        component:DashbordComponent
+    },
+    {
+        path:"Customer_Account",
+        component:CustomerAccountComponent
+    },
+    {
+        path:"Service",
+        component:ServiceComponent
+    }
+];
--- src/app/pages/nav-bar/nav-bar.component.ts
+++ src/app/pages/nav-bar/nav-bar.component.ts
@@ -1,12 +1,19 @@
+import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
-  imports: [],
+  imports: [RouterLink,NgClass],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.css'
 })
 export class NavBarComponent {
 
+  public SelectNavBar = 'Home';
+
+  public changeSelectNavBar(menuName:string){
+    this.SelectNavBar = menuName;
+  }
 }
