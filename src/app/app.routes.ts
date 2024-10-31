a5a3f154f0cf48efcceeae91193bbd9f169b7aeb
IsurikaDaniel <d.nethmiisurika@gmail.com>
2024-10-31 11:17:28 +0530

    new changes added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,10 @@ import { Routes } from '@angular/router';
 import { CustomerAccountComponent } from './pages/customer-account/customer-account.component';
 import { DashbordComponent } from './pages/dashbord/dashbord.component';
 import { ServiceComponent } from './pages/service/service.component';
+import { AppComponent } from './app.component';
+import { FeedbackComponent } from './pages/feedback/feedback.component';
+import { RuleAndRegulationsComponent } from './pages/rule-and-regulations/rule-and-regulations.component';
+import { AppointmentComponent } from './pages/appointment/appointment.component';
 
 export const routes: Routes = [
     {
@@ -15,5 +19,18 @@ export const routes: Routes = [
     {
         path:"Service",
         component:ServiceComponent
+    },
+    {
+        path:"Appointment",
+        component:AppointmentComponent
+    },
+    {
+        path:"Feedback",
+        component:FeedbackComponent
+    },
+    {
+        path:"Ruls",
+        component:RuleAndRegulationsComponent
     }
+
 ];
