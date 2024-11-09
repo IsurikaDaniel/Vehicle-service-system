0bb473599359345da2605170c5824ce3e4d2e9f2
IsurikaDaniel <d.nethmiisurika@gmail.com>
2024-11-10 01:09:09 +0530

    appointment backEnd & frontEnd merge

--- src/app/pages/appointment/appointment.component.ts
+++ src/app/pages/appointment/appointment.component.ts
@@ -1,12 +1,33 @@
+import { CommonModule } from '@angular/common';
+import { HttpClient,HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-appointment',
   standalone: true,
-  imports: [],
+  imports: [FormsModule,CommonModule,HttpClientModule],
   templateUrl: './appointment.component.html',
   styleUrl: './appointment.component.css'
 })
 export class AppointmentComponent {
+
+  public Appointment:any={
+    name:"",
+    vehicleType:"",
+    number:"",
+    email:"",
+    date:"",
+    location:"",
+    service:""
+  };
+
+  constructor(private http:HttpClient){}
+
+  public addAppointment(){
+    this.http.post("http://localhost:8080/Appointment/add-appointment",this.Appointment).subscribe((data)=>{
+        alert("Appointment Added!!!!");
+    })
+  }
 
 }
