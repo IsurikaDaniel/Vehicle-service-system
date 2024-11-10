c0b63abd2265823a1024914f9bca8a42ed8aeb24
IsurikaDaniel <d.nethmiisurika@gmail.com>
2024-11-10 23:51:56 +0530

    Account modification implementation

--- src/app/pages/admin-acconts-view/admin-acconts-view.component.ts
+++ src/app/pages/admin-acconts-view/admin-acconts-view.component.ts
@@ -1,9 +1,12 @@
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-admin-acconts-view',
   standalone: true,
-  imports: [],
+  imports: [FormsModule,CommonModule,HttpClientModule],
   templateUrl: './admin-acconts-view.component.html',
   styleUrl: './admin-acconts-view.component.css'
 })
