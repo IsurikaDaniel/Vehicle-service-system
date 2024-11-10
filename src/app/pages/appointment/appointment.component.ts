3c0c198410f4c8d3719b357804b331f3a8b71ecb
IsurikaDaniel <d.nethmiisurika@gmail.com>
2024-11-10 13:23:44 +0530

    appointment add completeded

--- src/app/pages/appointment/appointment.component.ts
+++ src/app/pages/appointment/appointment.component.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { HttpClient,HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-appointment',
@@ -12,22 +13,51 @@ import { FormsModule } from '@angular/forms';
 })
 export class AppointmentComponent {
 
-  public Appointment:any={
+  public appointment:any={
     name:"",
     vehicleType:"",
     number:"",
     email:"",
     date:"",
     location:"",
-    service:""
+    service:"",
+    paymentOption: "Cash Payments"
   };
 
   constructor(private http:HttpClient){}
 
-  public addAppointment(){
-    this.http.post("http://localhost:8080/Appointment/add-appointment",this.Appointment).subscribe((data)=>{
-        alert("Appointment Added!!!!");
-    })
+
+  public addAppointment() {
+    console.log("Sending appointment data:", this.appointment);
+    this.http.post("http://localhost:8080/Appointment/add-appointment", this.appointment).subscribe(
+      (res: any) => {
+        console.log(res);
+        if (res === true) {
+          Swal.fire({
+            position: "top-end",
+            icon: "success",
+            title: "Appointment Added!",
+            showConfirmButton: false,
+            timer: 1500
+          });
+        } else {
+          Swal.fire({
+            title: "success",
+            text: `Appointment Added!`,
+            icon: "success"
+          });
+        }
+      },
+      (error) => {
+        console.error("Error:", error);
+        Swal.fire({
+          title: "Error",
+          text: `Could not connect to the server. Please try again later.`,
+          icon: "error"
+        });
+      }
+    );
   }
+  
 
 }
