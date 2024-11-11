71e129dd585d4f83bc29c62e9f1f9c808c6ed07b
IsurikaDaniel <d.nethmiisurika@gmail.com>
2024-11-11 12:24:32 +0530

    account view updated

--- src/app/pages/admin-acconts-view/admin-acconts-view.component.ts
+++ src/app/pages/admin-acconts-view/admin-acconts-view.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
@@ -12,4 +12,39 @@ import { FormsModule } from '@angular/forms';
 })
 export class AdminAccontsViewComponent {
 
+  public accountList:any = [];
+
+  constructor(private http:HttpClient){
+     this.loadTable();  
+  }
+
+  loadTable(){
+    this.http.get("http://localhost:8080/CustomerAcc/get-all").subscribe(data=>{
+      console.log(data);
+      this.accountList=data;
+      
+    })
+  }
+
+  deleteAppointmentById(id:any){
+    console.log(id);
+
+    this.http.delete(`http://localhost:8080/CustomerAcc/delete-by-id/${id}`).subscribe(data=>{
+      alert("Appointment deleted !!!!");
+      this.loadTable();
+    })
+    
+}
+
+public accountTemp:any={}
+  updateAppointment(account:any){
+    console.log(account);
+    this.accountTemp=account;
+    
+  }
+  saveAppointment(){
+    this.http.put("http://localhost:8080/CustomerAcc/update-account",this.accountTemp).subscribe(data=>{
+      alert("Account Updated!!!!!")
+    })
+  }
 }
