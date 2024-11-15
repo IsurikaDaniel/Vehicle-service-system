266f2e5171ffbf94565269c922d2fa196e71c9f6
IsurikaDaniel <d.nethmiisurika@gmail.com>
2024-11-15 12:05:52 +0530

    Feedback component implementation

--- src/app/common/admin-feedback-view/admin-feedback-view.component.ts
+++ src/app/common/admin-feedback-view/admin-feedback-view.component.ts
@@ -1,13 +1,51 @@
 import { Component } from '@angular/core';
 import { AdminSidebarComponent } from '../admin-sidebar/admin-sidebar.component';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-admin-feedback-view',
   standalone: true,
-  imports: [AdminSidebarComponent],
+  imports: [AdminSidebarComponent,HttpClientModule,CommonModule,FormsModule],
   templateUrl: './admin-feedback-view.component.html',
   styleUrl: './admin-feedback-view.component.css'
 })
 export class AdminFeedbackViewComponent {
 
+  public FeedBackList:any = [];
+
+  constructor(private http:HttpClient){
+     this.loadTable();  
+  }
+
+  loadTable(){
+    this.http.get("http://localhost:8080/FeedBack/get-all").subscribe(data=>{
+      console.log(data);
+      this.FeedBackList=data;
+      
+    })
+  }
+
+  deleteFeedbackById(id:any){
+    console.log(id);
+
+    this.http.delete(`http://localhost:8080/FeedBack/delete-by-id/${id}`).subscribe(data=>{
+      alert("Feedback deleted !!!!");
+      this.loadTable();
+    })
+    
+}
+
+public FeedbackTemp:any={}
+  updateFeedback(Feedback:any){
+    console.log(Feedback);
+    this.FeedbackTemp=Feedback;
+    
+  }
+  saveUpdateFeedback(){
+    this.http.put("http://localhost:8080/FeedBack/update-feedBack",this.FeedbackTemp).subscribe(data=>{
+      alert("Feedback Updated!!!!!")
+    })
+  }
 }
--- src/app/pages/feedback/feedback.component.ts
+++ src/app/pages/feedback/feedback.component.ts
@@ -1,13 +1,56 @@
 import { Component } from '@angular/core';
 import { NavBarComponent } from '../nav-bar/nav-bar.component';
+import Swal from 'sweetalert2';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-feedback',
   standalone: true,
-  imports: [NavBarComponent],
+  imports: [NavBarComponent,FormsModule,CommonModule,HttpClientModule],
   templateUrl: './feedback.component.html',
   styleUrl: './feedback.component.css'
 })
 export class FeedbackComponent {
 
+  public Feedback:any={
+    description:"",
+    customerName:""
+  };
+
+  constructor(private http:HttpClient){}
+
+
+  public addFeedback() {
+    console.log("Sending appointment data:", this.Feedback);
+    this.http.post("http://localhost:8080/FeedBack/add-feedBack", this.Feedback).subscribe(
+      (res: any) => {
+        console.log(res);
+        if (res === true) {
+          Swal.fire({
+            position: "top-end",
+            icon: "success",
+            title: "FeedBack Added!",
+            showConfirmButton: false,
+            timer: 1500
+          });
+        } else {
+          Swal.fire({
+            title: "success",
+            text: `FeedBack Added!`,
+            icon: "success"
+          });
+        }
+      },
+      (error: any) => {
+        console.error("Error:", error);
+        Swal.fire({
+          title: "Error",
+          text: `Could not connect to the server. Please try again later.`,
+          icon: "error"
+        });
+      }
+    );
+  }
 }
