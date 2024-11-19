c0a668802c5051650108c773d8d7ce11856542c1
IsurikaDaniel <d.nethmiisurika@gmail.com>
2024-11-19 14:55:21 +0530

    email sender added

--- src/app/common/emailsender/emailsender.component.ts
+++ src/app/common/emailsender/emailsender.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { AdminDasbordComponent } from '../admin-dasbord/admin-dasbord.component';
+import { AdminSidebarComponent } from '../admin-sidebar/admin-sidebar.component';
 
 @Component({
   selector: 'app-emailsender',
   standalone: true,
-  imports: [],
+  imports: [AdminSidebarComponent],
   templateUrl: './emailsender.component.html',
   styleUrl: './emailsender.component.css'
 })
